ef747849d322c9c19dbaa4ba1642674caa850cb9
Edward Norton <edwardnorton@icloud.com>
2017-12-21 03:07:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
-  console.log(`Happy holidays, Katie!`);
+  console.log(`Happy holidays, ${name}!`);
   return "Happy holiday, ${name}!";
 }
 

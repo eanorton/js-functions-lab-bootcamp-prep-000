3c8e885477aa1075d786ec067d4e847ac8db6ec2
Edward Norton <edwardnorton@icloud.com>
2017-12-21 03:18:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,4 +17,5 @@ function happyHolidayTo(holiday, name) {
 
 function holidayCountdown(days, holiday) {
   console.log(`It's ${days} days until ${holiday}!`)
+  return "It\'s 20 days until Mother\'s Day!"
 }
